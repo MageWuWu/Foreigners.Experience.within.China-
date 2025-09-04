5c2419c83b2ec563a50f4c7fdec81c914d03a874
Wall Breaker <138102588+MageWuWu@users.noreply.github.com>
2025-09-04 23:47:24 +0800

    Update _meta.js

--- site/pages/docs/_meta.js
+++ site/pages/docs/_meta.js
@@ -1,5 +1,9 @@
 export default {
-  "Home": "index.md",
-  "Food": "Food-and-Ingredients.md",
-  "Transportation": "Transportation-and-Logistics.md"
+  "Home": "Home.md",
+  "Food and Ingredients": "Food-and-Ingredients.md",
+  "Transportation and Logistics": "Transportation-and-Logistics.md",
+  "Choose a Cost‐effectiveness Hotel": "Choose-a-Cost‐effectiveness-Hotel.md",
+  "Choose a reliable VPN service (CN)": "Choose-a-reliable-VPN-service-(CN).md",
+  "Religious and Cultural Diversity": "Religious-and-Cultural-Diversity.md",
+  "Reservation Guide": "Reservation-Guide.md"
 }
