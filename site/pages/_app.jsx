46cf226ccc7de293f0e9c81b315bfe788805a0cc
Wall Breaker <138102588+MageWuWu@users.noreply.github.com>
2025-09-04 23:47:56 +0800

    Update _app.jsx

--- site/pages/_app.jsx
+++ site/pages/_app.jsx
@@ -1,7 +1,2 @@
-// site/pages/_app.jsx
 import Nextra from 'nextra-theme-docs'
-
-export default Nextra({
-  // 这里可以传入您的主题配置，但它现在是可选的
-  // themeConfig: './theme.config.tsx'
-})
+export default Nextra({})
